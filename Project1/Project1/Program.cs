e6556bd3310c74db43be010201bf61586e70926e
mahmoudabdelfrarg <121587028+mahmoudabdelfrarg@users.noreply.github.com>
2022-12-29 02:32:50 +0200

    Add files via upload

---
+++ Project1/Project1/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+public class project1
+{
+    public static void Main()
+    {
+        Console.WriteLine("Project1: program to write the prime numbers from n1 to n2.");
+
+        int num, satrtnum, endnum, ctr, i;
+
+        Console.Write("1-Input starting number: ");
+
+        satrtnum = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("2-Input ending number: ");
+
+        endnum = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("3-The list of the prime numbers between {0} and {1} are : ", satrtnum, endnum);
+
+        for (num = satrtnum; num <= endnum; num++)
+        {
+            ctr = 0;
+
+            for (i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    ctr++;
+                    break;
+                }
+            }
+
+            if (ctr == 0 && num != 1)
+                Console.Write("{0} ", num);
+        }
+    }
+}
+        
+
---
+++ Project2/Project2/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+public class project2
+{
+    public static void Main()
+    {
+        Console.WriteLine("Project2: program to find the perfect numbers from n1 to n2");
+
+        int n, i, sum;
+       
+        int startnum, endnum;
+
+        Console.Write("1-Input the starting number: ");
+
+        startnum = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("2-Input the ending number: ");
+
+        endnum = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("3-The list of perfect numbers: ");
+
+        for (n = startnum; n <= endnum; n++)
+        {
+            i = 1;
+            sum = 0;
+            while (i < n)
+            {
+                if (n % i == 0)
+                    sum = sum + i;
+                i++;
+            }
+            if (sum == n)
+                Console.Write("{0} ", n);
+        }
+        
+    }
+}
